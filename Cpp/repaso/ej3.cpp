b4947c4a3c094577c924d9736e15a7a633412baf
patodehule1 <lucasgcastelli@gmail.com>
2026-03-06 14:47:59 -0300

    hice ejercicio 3

---
+++ Cpp/repaso/ej3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+const int N = 5;
+
+void mostrarMatriz(char matriz[N][N])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < N; i++)
+    {
+        cout << endl;
+        for (j = 0; j < N; j++)
+        {
+            cout << matriz[i][j];
+        }
+    }
+}
+
+void girarDerecha(char origen[N][N], char destino[N][N])
+{
+    int i;
+    int j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            destino[j][i] = origen[i][j];
+        }
+    }
+}
+
+void limpiarPantalla()
+{
+    for (int i = 0; i < 30; i++)
+    {
+        cout << endl;
+    }
+}
+
+int main()
+{
+    char matriz[N][N] = {
+        {'.', '.', '#', '.', '.'},
+        {'.', '#', '#', '#', '.'},
+        {'#', '#', '#', '#', '#'},
+        {'.', '.', '#', '.', '.'},
+        {'.', '.', '#', '.', '.'}};
+
+    char girada[N][N];
+
+    girarDerecha(matriz, girada);
+
+    cout << "Matriz original:\n\n";
+    mostrarMatriz(matriz);
+
+    cout << "\nPresionar Enter para girarla...";
+    cin.get();
+
+    limpiarPantalla();
+
+    cout << "Matriz girada 90 grados a la derecha:\n\n";
+    mostrarMatriz(girada);
+
+    return 0;
+}
