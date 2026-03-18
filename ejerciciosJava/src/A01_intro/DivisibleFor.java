e89b5629e029e6a9284f637e98d83b4c1a6a498c
patodehule1 <lucasgcastelli@gmail.com>
2026-03-18 09:33:27 -0300

    fix: fix the for start

--- ejerciciosJava/src/A01_intro/DivisibleFor.java
+++ ejerciciosJava/src/A01_intro/DivisibleFor.java
@@ -3,13 +3,13 @@ package A01_intro;
 public class DivisibleFor {
     public static void main(String[] args) {
         int i;
-        for(i = 0; i <= 100; i++) {
+        for(i = 1; i <= 100; i++) {
             if (i % 6 == 0) {
                 System.out.println(i);
             }
         }
 
-        i = 0;
+        i = 1;
 
         while(i <= 100){
             if (i % 6 == 0) {
